6c6c741afec0d784cf1d4d953057cb1a8c373b68
subham <majumdersubham14082003@gmail.com>
2026-05-25 00:29:32 +0530

    now complete

--- lower_upper_bound.go
+++ lower_upper_bound.go
@@ -1,1 +1,12 @@
-package goforcp
+v := make([]int,0)
+//lower_bound, first index where val >= x in log time
+lb := sort.Search(len(v), func(i int) bool {
+	return v[i] >= x
+})
+
+//upper_bound , first index where val > x in log time
+ub := sort.Search(len(v), func(i int) bool {
+	return v[i] > x
+})
+
+//to do it in map's keys make one key vector sort it and instead of v pass that key vector
