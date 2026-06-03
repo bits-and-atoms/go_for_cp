34088f10152b2741bd230439fe1c17fea9fd3b8a
subham <majumdersubham14082003@gmail.com>
2026-06-03 12:19:55 +0530

    while loop in stack

--- stack_queue.go
+++ stack_queue.go
@@ -22,3 +22,26 @@ top := st[len(st)-1]
 // Essential memory cleanup for pointer/object stacks:
 // st[len(st)-1] = nil
 st = st[:len(st)-1]
+
+//a previous and next greater element in stack and queue
+for i := 0; i < n; i++ {
+		pge[i] = -1
+		nge[i] = n
+		for len(st) > 0 && v[st[len(st)-1]] < v[i] {
+			top := st[len(st)-1]
+			nge[top] = i
+			st = st[:len(st)-1]
+		}
+		if len(st) > 0 {
+			pge[i] = st[len(st)-1]
+		}
+		st = append(st, i)
+	}
+	prev := n
+	for len(st) > 0 {
+		if prev != n && v[prev] == v[st[len(st)-1]] {
+			nge[st[len(st)-1]] = prev
+		}
+		prev = st[len(st)-1]
+		st = st[:len(st)-1]
+	}
